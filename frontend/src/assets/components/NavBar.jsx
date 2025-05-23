b621e86c1356da19ce467f2ee01419289e8aece1
Akarshanop <akarshantyagi@gmail.com>
2025-05-23 18:53:47 +0530

    added navbar and used tailwind

---
+++ frontend/src/assets/components/NavBar.jsx
@@ -0,0 +1,20 @@
+const NavBar = () => {
+  return (
+    <div class="flex ">
+      <div id="title" className=" mr-2">
+          FocusGate
+      </div>
+      <div id="dashboard" className=" mr-2">
+          Dashboard
+      </div>
+      <div id="question_setting" className=" mr-2">
+          Questions settings
+      </div>
+      <div id="profile" className="w-10 h-10   bg-blue-400  rounded-full">
+        
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
